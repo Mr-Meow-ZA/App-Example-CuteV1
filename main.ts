8b148bb6dfc6836486f6441e2a8a683b973aee58
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-05-24 14:11:41 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+microbitApp.onButtonOption(ButtonOption.Right, function () {
+    cuteBot.motors(25, -25)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff8000)
+    basic.pause(200)
+    cuteBot.closeheadlights()
+    basic.pause(200)
+})
+microbitApp.onButtonOption(ButtonOption.Down, function () {
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xff0000)
+    cuteBot.motors(microbitApp.getSliderX() * -1, microbitApp.getSliderZ() * -1)
+})
+microbitApp.onButtonReleased(function () {
+    cuteBot.stopcar()
+    cuteBot.closeheadlights()
+})
+microbitApp.onButtonOption(ButtonOption.Left, function () {
+    cuteBot.motors(-25, 25)
+    cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff8000)
+    basic.pause(200)
+    cuteBot.closeheadlights()
+    basic.pause(200)
+})
+microbitApp.onButtonOption(ButtonOption.Up, function () {
+    cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x00ff00)
+    cuteBot.motors(microbitApp.getSliderX(), microbitApp.getSliderZ())
+})
+cuteBot.stopcar()
+cuteBot.closeheadlights()
+basic.forever(function () {
+	
+})
